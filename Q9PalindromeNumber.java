b5c6baaa0505057a8102143db4f76fd5791170cc
harshalkgurjar <harshalgurjar.15@gmail.com>
2017-11-18 17:33:50 -0500

    Q9 Check if number is palindrome

---
+++ Q9PalindromeNumber.java
@@ -0,0 +1,25 @@
+public class Q9PalindromeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5765765;
+		//System.out.println(isWithExtraSpacePalindrome(a));
+		System.out.println(isPalindrome(a));
+	}
+
+	private static boolean isPalindrome(int x) {
+		// TODO Auto-generated method stub
+		// negative number cannot be palindrome
+		// also if last digit is zero then it cannot be a palindrome
+		if(x<0 || (x!=0 && x%10==0)){
+			return false;
+		}
+		//compare first half of digit with next half
+		int xRev=0;
+		while(x>xRev){
+			xRev=10*xRev + x%10;
+			x/=10;
+		}
+		return (x==xRev || x==xRev/10);
+	}
+}
